0da7b4784bd439df6fb52b8199797e6cb455e78f
Antoine De Gieter <antoine.degieter@net-production.ch>
2014-11-21 15:33:35 +0100

    add settings files

---
+++ SqeedTime/SettingsTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  SettingsTableViewCell.m
+//  SqeedTime
+//
+//  Created by Antoine De Gieter on 21/11/14.
+//  Copyright (c) 2014 Net Production Köbe & Co. All rights reserved.
+//
+
+#import "SettingsTableViewCell.h"
+
+@implementation SettingsTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
