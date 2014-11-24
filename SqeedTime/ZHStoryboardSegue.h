ff4ef8203897123e558c0be96b2fee711b510dcc
Antoine De Gieter <antoine.degieter@net-production.ch>
2014-11-24 10:26:03 +0100

    preparing for CATransition

---
+++ SqeedTime/ZHStoryboardSegue.h
@@ -0,0 +1,13 @@
+//
+//  ZHStoryboardSegue.h
+//  SqeedTime
+//
+//  Created by Antoine De Gieter on 24/11/14.
+//  Copyright (c) 2014 Net Production Köbe & Co. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZHStoryboardSegue : UIStoryboardSegue
+
+@end
---
+++ SqeedTime/ZHStoryboardSegue.m
@@ -0,0 +1,13 @@
+//
+//  ZHStoryboardSegue.m
+//  SqeedTime
+//
+//  Created by Antoine De Gieter on 24/11/14.
+//  Copyright (c) 2014 Net Production Köbe & Co. All rights reserved.
+//
+
+#import "ZHStoryboardSegue.h"
+
+@implementation ZHStoryboardSegue
+
+@end
